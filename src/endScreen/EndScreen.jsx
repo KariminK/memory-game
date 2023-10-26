1909e0ff30572c1ce27cd79c95eb82226bc5197e
KariminK <103779342+KariminK@users.noreply.github.com>
2023-10-26 19:30:16 +0200

    various css changes + RWD added

--- src/endScreen/EndScreen.jsx
+++ src/endScreen/EndScreen.jsx
@@ -2,7 +2,7 @@ import "./endscreen.css";
 const EndScreen = ({ lost, won, score, onPlayAgainClick, onRestartClick }) => {
   if (lost) {
     return (
-      <div className="endScreen">
+      <div className="endScreen loseScreen">
         <h1>*Woof* You have lost</h1>
         <h2>Your score was: {score}</h2>
         <button id="playAgainBtn" onClick={() => onPlayAgainClick(false)}>
@@ -12,15 +12,17 @@ const EndScreen = ({ lost, won, score, onPlayAgainClick, onRestartClick }) => {
     );
   } else if (won) {
     return (
-      <div className="endScreen">
+      <div className="endScreen winScreen">
         <h1>*Wow* You have won! You're god in memorizing shibes</h1>
         <h2>Your score was: {score}</h2>
-        <button id="playAgainBtn" onClick={() => onPlayAgainClick(true)}>
-          Play more
-        </button>
-        <button id="restartBtn" onClick={onRestartClick}>
-          Restart
-        </button>
+        <div className="endButtons">
+          <button id="playAgainBtn" onClick={() => onPlayAgainClick(true)}>
+            Play more
+          </button>
+          <button id="restartBtn" onClick={onRestartClick}>
+            Restart
+          </button>
+        </div>
       </div>
     );
   }
